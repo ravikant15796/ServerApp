1ba4ffb357a0e5f8c7364167eac70d44402c4a7b
ravikant15796 <ravikant15796@gmail.com>
2019-07-22 23:55:11 +0530

    changed origin

--- server/middlewares/middleware.js
+++ server/middlewares/middleware.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 module.exports = function(app){
     app.use(function(req,res,next){
         console.log(req.get('Origin'));
-        res.header("Access-Control-Allow-Origin", "http://localhost:4000");
+        res.header("Access-Control-Allow-Origin", "https://calm-taiga-50904.herokuapp.com");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         next();
     });
