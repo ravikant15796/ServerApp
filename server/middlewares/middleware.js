10f2f7aa5aff29a9576860b640ecb4be04fd2553
ravikant15796 <ravikant15796@gmail.com>
2019-07-23 21:19:28 +0530

    changed Access control

--- server/middlewares/middleware.js
+++ server/middlewares/middleware.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 module.exports = function(app){
     app.use(function(req,res,next){
         console.log(req.get('Origin'));
-        res.header("Access-Control-Allow-Origin", "https://calm-taiga-50904.herokuapp.com");
+        res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         next();
     });
