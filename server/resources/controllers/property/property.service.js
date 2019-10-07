ad7f51a6af06abe8e96823e9cfe0f4bd0cb6ae52
ravikant15796@gmail.com <ravikant15796@gmail.com>
2019-10-07 23:31:15 +0530

    added lean for faster queries

--- server/resources/controllers/property/property.service.js
+++ server/resources/controllers/property/property.service.js
@@ -7,14 +7,14 @@ const Property = mongoose.model("Property");
 class PropertyService {
     async getPopularProperties(query){
         query = JSON.parse(JSON.stringify(query));
-        return  await Property.find({citySlug:query.citySlug,enabled:true},{name:1, countrySlug:1, slug:1, citySlug:1, minPrice:1, media:1}).limit(8);
+        return  await Property.find({citySlug:query.citySlug,enabled:true},{name:1, countrySlug:1, slug:1, citySlug:1, minPrice:1, media:1}).limit(8).lean(true);
     }
     async getProperties(citySlug,page){
-        return  await Property.find({citySlug:citySlug,enabled:true}).skip((parseInt(page)-1)*10).limit(10);
+        return  await Property.find({citySlug:citySlug,enabled:true}).skip((parseInt(page)-1)*10).limit(10).lean(true);
     }
     async getPropertyDetails(query){
         query = JSON.parse(JSON.stringify(query));
-        return await Property.findOne({slug:query.propertySlug});
+        return await Property.findOne({slug:query.propertySlug}).lean(true);
     }
     async findProperty(name) {
         return Property.find({
@@ -22,7 +22,7 @@ class PropertyService {
                 "$regex": name, "$options": "i"
             },
             "enabled": true
-        }).select("name city slug country citySlug countrySlug").limit(10);
+        }).select("name city slug country citySlug countrySlug").limit(10).lean(true);
     }
 }
 
